43face4b38bccce3106d8d2cdc8a179da926f8b5
Jichi Guo <jichifly@gmail.com>
2026-01-31 01:25:31 -0800

    save

--- test_inference_testset.sh
+++ test_inference_testset.sh
@@ -8,9 +8,18 @@
 #    --num-scenes 10 \
 #    --verbose
 
-python test_inference_testset_v3.py output_test_4ch/epoch100_XRD075MG8/model_00100 \
+#python test_inference_testset_v3.py output_test_4ch/epoch100_XRD075MG8/model_00100 \
+#    --config config/bathrooms_test_config_4ch.yaml \
+#    --json-dir data/bathroom_2.2k_filter \
+#    --num-scenes 10 \
+#    --split train \
+#    --verbose
+
+#python test_inference_testset_v3.py output_test_4ch/ZHUUDNPZT/model_00000
+python test_inference_testset_v3.py output_test_4ch_gpu/output_test_4ch/GVAVUGQNU/model_00000 \
     --config config/bathrooms_test_config_4ch.yaml \
     --json-dir data/bathroom_2.2k_filter \
     --num-scenes 10 \
     --split train \
     --verbose
+
--- test_train_4ch_gpu.sh
+++ test_train_4ch_gpu.sh
@@ -3,4 +3,4 @@
 #export DYLD_LIBRARY_PATH=/opt/homebrew/opt/libomp/lib:$DYLD_LIBRARY_PATH
 #export LDFLAGS="-L/opt/homebrew/opt/libomp/lib"
 #export CPPFLAGS="-I/opt/homebrew/opt/libomp/include"
-python scripts/train_network.py config/bathrooms_test_config_4ch_gpu.yaml output_test_4ch/
+python scripts/train_network.py config/bathrooms_test_config_4ch_gpu.yaml output_test_4ch_gpu/
