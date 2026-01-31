21c3bed206fbbc56e903bc4528161de4c81aeafb
Jichi Guo <jichifly@gmail.com>
2026-01-31 02:10:30 -0800

    save

--- test_inference_testset.sh
+++ test_inference_testset.sh
@@ -15,11 +15,11 @@
 #    --split train \
 #    --verbose
 
-#python test_inference_testset_v3.py output_test_4ch_gpu/UR4P9V3VP/model_00009 \
-python test_inference_testset_v3.py output_test_4ch/GP0ODX41W/model_00000 \
+#python test_inference_testset_v3.py output_test_4ch/GP0ODX41W/model_00000 \
+python test_inference_testset_v3.py output_test_4ch_gpu/QCPVUZBIE/model_00020 \
     --config config/bathrooms_test_config_4ch.yaml \
     --json-dir data/bathroom_2.2k_filter \
-    --num-scenes 10 \
+    --num-scenes 20 \
     --split train \
     --verbose
 
