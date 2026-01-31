406233fe889fd997f95bc68cf58d57a7538e6a7a
Jichi Guo <jichifly@gmail.com>
2026-01-31 04:19:48 -0800

    ave

--- test_inference_testset.sh
+++ test_inference_testset.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -v
-python test_inference_testset_v5.py output_test_4ch/0M3W2MIJN/model_00001 \
+python test_inference_testset_v5.py output_test_4ch/0M3W2MIJN/model_00004 \
     --config config/bathrooms_test_config_4ch.yaml \
     --json-dir data/bathroom_2.2k_filter \
     --num-scenes 5 \
