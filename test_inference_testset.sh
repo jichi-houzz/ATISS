75f07990ad759ae57bed7379fdefb3eb8741f795
Jichi Guo <jichifly@gmail.com>
2026-01-31 03:07:37 -0800

    save

--- test_inference_testset.sh
+++ test_inference_testset.sh
@@ -2,7 +2,7 @@
 python test_inference_testset_v4.py output_test_4ch/EA7AZJE4X/model_00000 \
     --config config/bathrooms_test_config_4ch.yaml \
     --json-dir data/bathroom_2.2k_filter \
-    --num-scenes 20 \
+    --num-scenes 5 \
     --split train \
     --verbose
 
