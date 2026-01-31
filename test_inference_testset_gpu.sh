313045081d2734309b01cb932720472223229201
Jichi Guo <jichifly@gmail.com>
2026-01-31 04:24:13 -0800

    save

--- test_inference_testset_gpu.sh
+++ test_inference_testset_gpu.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -v
-python test_inference_testset_v5.py output_test_4ch_gpu/N9IKZT69Y/model_00000 \
+python test_inference_testset_v5.py output_test_4ch_gpu/ME75C31BI/model_00000 \
     --config config/bathrooms_test_config_4ch_gpu.yaml \
     --json-dir data/bathroom_2.2k_filter \
     --num-scenes 20 \
