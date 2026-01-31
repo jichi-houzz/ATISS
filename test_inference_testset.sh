c49e93f9cd913c9319d8d382c6232e64f2c3f5ad
Jichi Guo <jichifly@gmail.com>
2026-01-30 22:16:23 -0800

    sve

--- test_inference_testset.sh
+++ test_inference_testset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -v
-python test_inference_testset.py output_test/WI3NACUD1/model_00001 \
+python test_inference_testset.py output_test/epoch50_N3BO74H4M/model_00001 \
     --num-scenes 10 \
     --verbose
